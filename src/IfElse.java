0ecbe9bd9a9d4888f770ec1a55c075bc3d7c03c5
JoaoHypo <jguibarboza@gmail.com>
2023-07-06 14:16:52 -0300

    If Else saudação

--- src/IfElse.java
+++ src/IfElse.java
@@ -11,7 +11,7 @@ public class IfElse {
         if (hora < 12) {
             System.out.println("Bom dia");
 
-        } else if (hora < 18 ){
+        } else if (hora < 18 ){ // como ja passou o primeiro bloco, ja vai incluir o hora == 12
             System.out.println("Boa tarde");
 
         }else {
