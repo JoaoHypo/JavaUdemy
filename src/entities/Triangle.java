297499ea8e65dd7ba5998d6a75e440d224c2026e
JoaoHypo <jguibarboza@gmail.com>
2023-07-09 13:16:46 -0300

    CLASSE = TIPO OBJETO =  INSTÂNCIA

---
+++ src/entities/Triangle.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class Triangle {
+    // CLASSE É UM TIPO, TAL QUAL INT, BOOL, W/e
+    //declarando atributos, membros
+    public double a;
+    public double b;
+    public double c;
+    }
