827e517bf3053c06c64d0ff8e50d330eced09474
JoaoHypo <jguibarboza@gmail.com>
2023-07-13 23:38:54 -0300

    Few Ajusts

---
+++ src/OOB/RectangleEx1.java
@@ -0,0 +1,22 @@
+package OOB;
+
+import entities.Rectangle;
+
+import java.util.Scanner;
+
+public class RectangleEx1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Rectangle rec1 = new Rectangle();
+
+        System.out.println("Enter rectangle width and height:");
+
+        rec1.width = sc.nextDouble();
+        rec1.height = sc.nextDouble();
+
+        System.out.println(rec1);
+
+        sc.close();
+    }
+}
