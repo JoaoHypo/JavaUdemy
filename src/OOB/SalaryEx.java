fe1941c52b00562447519e71d20c2f8adbdc1493
JoaoHypo <jguibarboza@gmail.com>
2023-07-13 23:48:11 -0300

    Employee excercise

---
+++ src/OOB/SalaryEx.java
@@ -0,0 +1,16 @@
+package OOB;
+
+import entities.Employee;
+
+import java.util.Scanner;
+
+public class SalaryEx {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Employee E1 = new Employee();
+
+
+        sc.close();
+    }
+}
