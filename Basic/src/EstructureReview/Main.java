215a1e87c43f64e1c12683933d1781c0e93d6184
JoaoHypo <jguibarboza@gmail.com>
2023-07-16 23:36:59 -0300

    Files management

--- Basic/src/EstructureReview/Main.java
+++
@@ -1,7 +0,0 @@
-package EstructureReview;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- Basic/src/entities/CurrencyConverter.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-public class CurrencyConverter {
-    public static double buy(double price,double many){
-        return price*many*1.06;
-    }
-
-}
