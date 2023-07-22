3c1514bfa04d62a146fdead5cb834c9d3e287e9b
JoaoHypo <jguibarboza@gmail.com>
2023-07-21 23:35:44 -0300

    correction

--- src/EstructureReview/Vectors_Students.java
+++ src/EstructureReview/Vectors_Students.java
@@ -12,6 +12,7 @@ public class Vectors_Students {
         String[] vect = new String[10];
 
         for (int i = 0; i < n ; i++){
+            System.out.println();
             System.out.printf("RENT NUMBER #%d: %n",i+1);
             System.out.print("Name: ");
             String name = sc.nextLine();
