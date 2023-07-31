a350ea26b3a362c671beb1949957c4a16aef0382
JoaoHypo <jguibarboza@gmail.com>
2023-07-30 21:47:17 -0300

    Understanding while with false.

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         List<String> lista = new ArrayList<String>();
+
+        Scanner sc = new Scanner(System.in);
 
         lista.add("Teste");
         lista.add("Teste2");
@@ -13,6 +16,21 @@ public class Test {
         System.out.println(lista.stream().filter(x -> x.startsWith("J")).findFirst().orElse(null));
         System.out.println(lista);
 
+        int x = 12;
+        int y = 10;
 
+        while(test(x,y)){
+            x = sc.nextInt();
+            System.out.println(test(x,y));
+        }
+        while(x != y){
+            System.out.println("Teste");
+        }
+
+
+        sc.close();
+    }
+    public static boolean test(int x,int y){
+        return x != y;
     }
 }
