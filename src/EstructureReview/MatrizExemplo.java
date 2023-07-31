5ce2a5d434a8d5013fc96708835315749d4d5510
JoaoHypo <jguibarboza@gmail.com>
2023-07-31 19:49:04 -0300

    printing matrix

--- src/EstructureReview/MatrizExemplo.java
+++ src/EstructureReview/MatrizExemplo.java
@@ -41,5 +41,14 @@ public class MatrizExemplo {
             }
         }
     }
+    // Função para imprimir a matriz
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
