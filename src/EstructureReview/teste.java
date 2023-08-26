3489d05e18cb1253b0d15c87ae111b92ea47dcb4
JoaoHypo <jguibarboza@gmail.com>
2023-08-25 22:31:38 -0300

    test with .clone()

--- src/EstructureReview/teste.java
+++ src/EstructureReview/teste.java
@@ -1,10 +1,17 @@
-package EstructureReview;
+import java.util.Arrays;
 
 public class teste {
     public static void main(String[] args) {
+        int[] originalArray = {1, 2, 3, 4, 5};
 
-        System.out.println("TesteGit");
-        System.out.println("Teste 2, other way");
+        // Cloning the original array
+        int[] clonedArray = originalArray; //comentado o clone .clone();
 
+        // Modifying the original array
+        originalArray[0] = 10;
+
+        // Printing both arrays
+        System.out.println("Original Array: " + Arrays.toString(originalArray));
+        System.out.println("Cloned Array: " + Arrays.toString(clonedArray));
     }
 }
