10b821755cbed081e8b11ebc0f5ba82dee8c1fe6
JoaoHypo <jguibarboza@gmail.com>
2023-09-20 23:37:28 -0300

    Creating folder

--- Files/src/application/Folders.java
+++ Files/src/application/Folders.java
@@ -7,12 +7,26 @@ public class Folders {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a folder path:\n --> ");
+        System.out.print("Enter a folder path:\n --> ");
         String strPath = sc.nextLine();
 
         File path = new File(strPath);
 
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS: ");
 
+        for (File folder : folders){
+            System.out.println(folder);
+        }
+
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("FILES:");
+        for (File file : files){
+            System.out.println(file);
+        }
+
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
 
         sc.close();
     }
