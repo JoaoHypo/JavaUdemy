9110bfe31bb03bf951e914e2bf733b1301eed363
JoaoHypo <jguibarboza@gmail.com>
2023-11-08 12:10:37 -0300

    Func -> Func

---
+++ FuncionalProgLambdaExpress/src/Lambda5/model/services/ProductService.java
@@ -0,0 +1,18 @@
+package Lambda5.model.services;
+import java.util.List;
+import java.util.function.Predicate;
+
+import Lambda5.model.entities.Product;
+
+public class ProductService {
+
+	public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+		double sum = 0.0;
+		for (Product p : list) {
+			if (criteria.test(p)) {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
+}
